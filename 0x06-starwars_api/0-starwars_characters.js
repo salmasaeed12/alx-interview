1064420c4c45071c9284d2031c31b5aa02b0f04a
root <root@DESKTOP-IGJR8BT.localdomain>
2024-09-24 05:21:27 +0300

    5:16

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
+const baseUrl = 'https://swapi-api.alx-tools.com/api/films/';
 
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-      req(arr, i + 1);
-    }
+async function requestAsync (url) {
+  return new Promise((resolve, reject) => {
+    request(url, function (err, _, body) {
+      if (err) return reject(err);
+      resolve(JSON.parse(body));
+    });
   });
-};
-
-request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-      req(chars, 0);
-    }
+}
+async function main () {
+  const film = await requestAsync(baseUrl + process.argv[2]);
+  for (const url of film.characters) {
+    const character = await requestAsync(url);
+    console.log(character.name);
   }
-);
+}
+main();
